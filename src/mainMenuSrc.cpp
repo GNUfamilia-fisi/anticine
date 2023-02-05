c553963ce689f3ea272150bc6ab47163c3613cfa
PandaDiestro <destructo900@gmail.com>
2023-02-04 23:41:15 -0500

    logo workaround and color enum at display utils

--- src/mainMenuSrc.cpp
+++ src/mainMenuSrc.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <windows.h>
+#include <thread>
+#include <chrono>
+
 #include "utilsDisplay.h"
 
+void logoDisplay(DWORD color);
+
+HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+//refer to utisDisplay.h for the complete list of foreground colors
+
+
+
 int main(){
-    std::cout << "CiNext jeje" << std::endl;
+    system("cls");
+
+    DWORD color = 1;
+
+
+    //main menu loop
+    while(1){
+        gotoXY(0,0);
+        
+        logoDisplay(RED);
+
+        color++;
+
+        if(color == 10){
+            //color = 1;
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
     return 0;
+}
+
+
+void logoDisplay(DWORD color){
+    SetConsoleTextAttribute(consoleHandle, color);
+    std::cout << R"(
+     $$$$$$\  $$\ $$\   $$\ $$$$$$$$\ $$\   $$\ $$$$$$$$\ 
+    $$  __$$\ \__|$$$\  $$ |$$  _____|$$ |  $$ |\__$$  __|
+    $$ /  \__|$$\ $$$$\ $$ |$$ |      \$$\ $$  |   $$ |   
+    $$ |      $$ |$$ $$\$$ |$$$$$\     \$$$$  /    $$ |   
+    $$ |      $$ |$$ \$$$$ |$$  __|    $$  $$<     $$ |   
+    $$ |  $$\ $$ |$$ |\$$$ |$$ |      $$  /\$$\    $$ |   
+    \$$$$$$  |$$ |$$ | \$$ |$$$$$$$$\ $$ /  $$ |   $$ |   
+     \______/ \__|\__|  \__|\________|\__|  \__|   \__|   
+    )";
 }
--- src/utilsDisplay.h
+++ src/utilsDisplay.h
@@ -3,4 +3,24 @@
 
 void gotoXY(int x, int y);
 
+enum ForegroundColors {
+    BLACK = 0,
+    BLUE = 1,
+    GREEN = 2,
+    CYAN = 3,
+    RED = 4,
+    MAGENTA = 5,
+    BROWN = 6,
+    LIGHTGRAY = 7,
+    GRAY = 8,
+    LIGHTBLUE = 9,
+    LIGHTGREEN = 10,
+    LIGHTCYAN = 11,
+    LIGHTRED = 12,
+    LIGHTMAGENTA = 13,
+    YELLOW = 14,
+    WHITE = 15
+};
+
+
 #endif
