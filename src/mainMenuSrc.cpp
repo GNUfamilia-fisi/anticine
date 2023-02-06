ae571a9b1163607d561c8bbb0773a3e72426fc6a
PandaDiestro <destructo900@gmail.com>
2023-02-06 16:02:11 -0500

    changed for vertical scrolling

--- src/mainMenuSrc.cpp
+++ src/mainMenuSrc.cpp
@@ -5,22 +5,27 @@ using json = nlohmann::json;
 
 void logoDisplay(DWORD color);
 
-void cinemaDisplay(json closeData);
+void cinemaDisplay(json closeData, int current, int namePos);
+
+void colorLine(DWORD color, int y);
 
 HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 
 std::string cinemaID;
 
-json closeCineData = fetch("https://a195-181-66-156-128.sa.ngrok.io/cines/cercanos");
+json closeCineData = fetch("https://27a0-181-66-156-128.sa.ngrok.io/cines/cercanos");
 
 //refer to utisDisplay.h for the complete list of foreground colors
 
 
 int main(){
 
-    short currentCine = 0;
+    int currentCine = 0;
+    int nameCursorPos = 0;
 
     DWORD color = 0;
+
+    char lock = 0;
     
     system("cls");
     ShowConsoleCursor(false);
@@ -40,23 +45,33 @@ int main(){
 
         std::cout << std::endl;
 
-        if (GetAsyncKeyState(VK_RIGHT)){
+        if(GetAsyncKeyState(VK_DOWN)){
             if(currentCine < closeCineData["cinemas"].size() - 1){
-                currentCine++;
+                currentCine++;    
+            }
+
+            if (nameCursorPos < 4){
+                nameCursorPos++;
+            }
+
+            else{
+                nameCursorPos = 4;
+            }
+        }
+        
+        if(GetAsyncKeyState(VK_UP)){
+            if(currentCine > 0){
+                currentCine--;
             }
         }
 
-        else if(GetAsyncKeyState(VK_LEFT) && currentCine > 0){
-            currentCine--;
-        }
-
-        else if(GetAsyncKeyState(VK_RETURN)){
+        if(GetAsyncKeyState(VK_RETURN)){
             cinemaID = closeCineData["cinemas"][currentCine]["cinema_id"].get<std::string>();
         }
 
-        cinemaDisplay(closeCineData["cinemas"][currentCine]["name"]);
+        cinemaDisplay(closeCineData["cinemas"], currentCine, nameCursorPos);
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
     return 0;
@@ -79,18 +94,59 @@ void logoDisplay(DWORD color){
 }
 
 
-void cinemaDisplay(json closeData){
+void cinemaDisplay(json closeData, int current, int namePos){
     SetConsoleTextAttribute(consoleHandle, WHITE);
 
-    std::string displayed = "> " + closeData.get<std::string>() + " <";
+    int showSize = 5;
+    int indic = namePos;
 
-    int Xpos = (getConsoleRectSize().x / 2) - (displayed.length() / 2);
+    if(current < showSize){
+        indic = current;
+    }
 
-    gotoXY(0, 12);
-    cleanLine();
+    std::string names[showSize];
+    
+    if (current < showSize){
+        for(int i = 0; i < showSize; i++){
+            names[i] = closeData[i]["name"].get<std::string>();
+        }
+    }
 
-    gotoXY(Xpos, 12);
-    std::cout << displayed;
+    else{
+        for(int i = 0; i < showSize; i++){
+            names[i] = closeData[i + current - showSize + 1]["name"].get<std::string>();
+        }
+    }
 
-    gotoXY(Xpos + 3, 12);
+    //cleaner
+    for (int i = 0; i < showSize; i++){
+        gotoXY(0, 12 + i);
+        cleanLine();
+    }
+
+
+    for (int i = 0; i < showSize; i++){
+
+        if (i == indic){
+            gotoXY(
+                (getConsoleRectSize().x / 2) - names[i].length() - 1,
+                12 + i
+            );
+
+            SetConsoleTextAttribute(consoleHandle, LIGHTGREEN);
+
+            std::cout << ">>";
+        }
+
+        else{
+            SetConsoleTextAttribute(consoleHandle, WHITE);
+        }
+
+        gotoXY(
+            (getConsoleRectSize().x / 2) - (names[i].length() / 2),
+            12 + i
+        );
+
+        std::cout << names[i];
+    }
 }
